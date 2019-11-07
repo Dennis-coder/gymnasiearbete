57160f67cba04948099022081f8aa36cedc75339
potatislc <lucalbin@gmail.com>
2019-11-07 11:33:40 +0100

    Placing towers
    Placing towers

--- Tower_Defense/scripts/MouseController.cs
+++ Tower_Defense/scripts/MouseController.cs
@@ -8,21 +8,37 @@ public class MouseController : Node2D
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
+    Node2D cellHighlight;
+    public TileMap worldGrid;
+    PackedScene towerScene;
+
+    Vector2 gridPos;
+
     public override void _Ready()
     {
-        
+        cellHighlight = GetNode<Node2D>("Cell Highlight");
+        // get_tree().get_root().get_node("myRootNode").find_node("desiredNode")
+        worldGrid = GetTree().GetRoot().GetNode("World").FindNode("WorldGrid") as TileMap;
+        towerScene = (PackedScene)ResourceLoader.Load("res://Scenes/Towers/Tower.tscn");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-    
+        gridPos = worldGrid.MapToWorld(worldGrid.WorldToMap(GetViewport().GetMousePosition()));
+
+        cellHighlight.SetPosition(gridPos);
     }
 
     public override void _Input(InputEvent @event) {
     	InputEventMouseButton e = @event as InputEventMouseButton;
     	if (e != null) {
-    		GD.Print("ye");
+            if (e.IsActionPressed("mouse_left")) {
+
+                var tower = towerScene.Instance() as Node2D;
+                tower.SetPosition(gridPos);
+                AddChild(tower);
+            }
     	}
     }
 }   
