679b9e672a240c78d19db91853f6f3e9d86521d6
lukaskuh <55488888+lukaskuh@users.noreply.github.com>
2020-02-27 16:12:10 +0100

    BombTrajectory
    bruh del 1 asså

--- Tower_Defense/scripts/Bomb.cs
+++ Tower_Defense/scripts/Bomb.cs
@@ -5,11 +5,14 @@ public class Bomb : Projectile
 {
     public Vector2 origin;
     public Vector2 targetPos;
-
     PackedScene explosionScene;
+
+    Node2D pellet;
+
 
     public override void _Ready() {
         base._Ready();
+        pellet = GetNode<Node2D>("Pellet");
 
         explosionScene = (PackedScene)ResourceLoader.Load("res://Scenes/Effects/Explosion.tscn");
     }
@@ -21,6 +24,26 @@ public class Bomb : Projectile
 
         if (origin.DistanceTo(targetPos) <= origin.DistanceTo(GetPosition())) {
             Hit();
+        }
+
+        float lT = 0f;
+
+        float progress = origin.DistanceTo(GetPosition())/origin.DistanceTo(targetPos);
+
+        
+
+        
+
+        if (origin.DistanceTo(GetPosition()) < origin.DistanceTo(targetPos)) {
+            pellet.SetScale(GetScale() + new Vector2(.1f, .1f));
+            Vector2 oPos = new Vector2(0,0);
+            Vector2 pPos = oPos.LinearInterpolate(oPos + new Vector2(0, -50), progress*2);
+            pellet.SetPosition(pPos);
+        } else {
+            pellet.SetScale(GetScale() - new Vector2(.1f, .1f));
+            Vector2 oPos = new Vector2(0,-50);
+            Vector2 pPos = oPos.LinearInterpolate(oPos + new Vector2(0, 0), progress*2);
+            pellet.SetPosition(pPos);
         }
 
     }
